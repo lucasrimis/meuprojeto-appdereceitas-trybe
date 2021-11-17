f2e090ce1d0b0ffe0b7f306672cae88234c54133
VSSSP <victorssspaula@live.com>
2021-11-17 14:29:51 -0300

    Requisitos 56 e 57

--- src/pages/Receitas/ReceitasFeitas.js
+++ src/pages/Receitas/ReceitasFeitas.js
@@ -1,16 +1,24 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Header from '../../components/Header';
 import MyContext from '../../Context';
 import { getDoneRecipes } from '../../services/helpers/doneRecipes';
 import shareIcon from '../../images/shareIcon.svg';
 
+const copy = require('clipboard-copy');
+
 export default function ReceitasFeitas() {
   const { setPageName, setShowButton } = useContext(MyContext);
+  const [copied, setCopied] = useState(false);
 
   useEffect(() => {
     setPageName('Receitas Feitas');
     setShowButton(false);
   }, [setPageName, setShowButton]);
+
+  const handleCopy = (type, id) => {
+    copy(`http://localhost:3000/${type}s/${id}`);
+    setCopied(true);
+  };
 
   const renderRecipes = () => {
     const getRecipes = getDoneRecipes();
@@ -22,7 +30,13 @@ export default function ReceitasFeitas() {
           data-testid={ `${index}-horizontal-image` }
           width="150px"
         />
-        <p data-testid={ `${index}-horizontal-top-text` }>{`${recipe.area} - ${recipe.category}`}</p>
+        <p
+          data-testid={ `${index}-horizontal-top-text` }
+        >
+          {`${recipe.type === 'comida' ? `${recipe.area} - ` : ''} ${recipe.category}`}
+          {' '}
+          {`${recipe.type === 'bebida' ? recipe.alcoholicOrNot : ''}`}
+        </p>
         <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
         <p
           data-testid={ `${index}-horizontal-done-date` }
@@ -33,9 +47,11 @@ export default function ReceitasFeitas() {
           type="button"
           data-testid={ `${index}-horizontal-share-btn` }
           src={ shareIcon }
+          onClick={ () => handleCopy(recipe.type, recipe.id) }
         >
           <img src={ shareIcon } alt="" />
         </button>
+        { copied ? <p>Link copiado!</p> : null }
         <span>
           Tags:
           { recipe.tags.map((tag, i) => (
